4888abfacb7338675dfc0b95ea40b0aa9c6f6ba5
Anna <mambarinka@mail.ru>
2021-02-08 15:47:22 +0400

    feat: implements Fn without stack

--- src/index.js
+++ src/index.js
@@ -1,51 +1,23 @@
-  
-// module.exports = function check(str, bracketsConfig) {
-// };
+module.exports = function check(str, bracketsConfig) {
+    let configArray = bracketsConfig.map((i) => i[0] + i[1]); // создание одномерного массива из конфига
 
-// function check(str, bracketsConfig) {
+    let newStr = str;
 
-// }
-
-
-let isBalanced = (input, config) => {
-
-    let brackets = "[]{}()<>||";
-    let stack = [];
-  
-    for (let bracket of input) {
-      let bracketsIndex = brackets.indexOf(bracket);
-  
-      if (bracketsIndex % 2 === 0) {
-        stack.push(bracketsIndex + 1);
-        console.log(`bracketsIndex: ${bracketsIndex}`);
-        console.log(`stack: ${stack}`);
-      } else {
-  
-        if (stack.pop() !== bracketsIndex) {
-          console.log(false);
-          return false;
+    while (true) { // пока массив не закончится
+        for (let i = 0; i < configArray.length; i++) {
+            str = str.replace(configArray[i], ''); // поиск нужного элемента в строке и замена его на ''
         }
-      }
+
+        if (str.length == 0) {
+            return true;
+        }
+
+
+        if (newStr === str) { // элемент не удален, т.е. нет совпадения из configArray
+            return false;
+        }
+        //   console.log(`newStr: ${newStr}`);
+        //   console.log(`str: ${str}`);
+        newStr = str;
     }
-  console.log(stack.length === 0);
-    return stack.length === 0;
-  };
-  
-  isBalanced('||');
-  
-  let array = [
-    ['(', ')'],
-    ['[', ']']
-  ];
-  let object = {};
-  
-  // for (let item of array) {
-  //   object[item[0]] = item[1];
-  // }
-  
-  object = array.reduce((acc, item) => {
-    acc[item[0]] = item[1];
-    return acc;
-  }, {});
-  
-  console.log(object);
+};
